7aa74d85f65edb727d77ad91843c746a9893ec8b
zhangjiwei <1826307398@qq.com>
2026-03-09 15:12:51 +0800

    dev: 支持配置翻译目标语言

--- test/index.js
+++ test/index.js
@@ -6,4 +6,5 @@ process({
     dir: path.join(__dirname, "source"),
     output: path.join(__dirname, "source/output.json"),
     apikey: "368f5036ec4c88c2afb9a0705d53c384", //必填：小牛翻译的apikey，单日赠送200000字符翻译额度，可以自己去官网申请
+    to: ["en","cht"] //英文、中文繁体
 })
